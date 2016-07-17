234bae51b50ce12e86021c8e78bc6062af8b0374
elf-mouse <ifmouz@gmail.com>
2016-07-18 06:10:29 +0800

    update

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -2,6 +2,10 @@ function sayHello(msg) {
   console.log('Hello ' + msg);
 }
 
+// ES5
+// module.exports.sayHello = sayHello;
+
+// ES6
 export {
   sayHello
 };
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
-import { sayHello } from './common';
+// ES5
+// const common = require('./common');
+// common.sayHello('World');
 
+// ES6
+import { sayHello } from './common';
 sayHello('World');
